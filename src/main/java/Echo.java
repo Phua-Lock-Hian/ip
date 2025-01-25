25943e035c6e687e9f3b0cb6f87c13f2a17537c6
Phua-Lock-Hian <e1157347@u.nus.edu>
2025-01-25 13:25:36 +0800

    Level 1 Echo added

---
+++ src/main/java/Echo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Echo {
+    public void start() {
+        String line;
+        Scanner in = new Scanner(System.in);
+        do {
+            line = in.nextLine();
+            System.out.println(line); //echo
+            System.out.println("--");
+        } while (!line.equals("bye"));
+
+        //loop exits when you type bye
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("--");
+        in.close();
+    }
+}
--- src/main/java/Swing.java
+++ src/main/java/Swing.java
@@ -1,10 +1,13 @@
+//import java.util.Scanner;
+
 public class Swing {
     public static void main(String[] args) {
         System.out.println("--");
-        System.out.println("Hello! I'm Swing");
+        System.out.println("Hello! I'm Swing ^_^");
         System.out.println("What can I do for you?");
         System.out.println("--");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("--");
+
+        Echo echo = new Echo();
+        echo.start();
     }
 }

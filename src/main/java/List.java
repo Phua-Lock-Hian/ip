b71d43e217c93c908c26d728454a68f88b79a3cb
Phua-Lock-Hian <e1157347@u.nus.edu>
2025-01-25 15:15:58 +0800

    Level 3 Tasks, Marking and Unmarking added. Also added customized responses.

--- src/main/java/List.java
+++ src/main/java/List.java
@@ -1,32 +1,56 @@
 import java.util.Scanner;
 
 public class List {
-    private String[] list;
+    private Task[] list;
     private int len;
+
     public List() {
-        list = new String[100];
+        list = new Task[100];
         len = 0;
     }
+
     public void start() {
         String line;
         Scanner in = new Scanner(System.in);
         do {
-            line = in.nextLine();
-
-            if (line.equals("list")) {
-                for (int i = 0; i < len; i++) {
-                    System.out.println( (i + 1)  + ". " + list[i]);
-                    System.out.println("--");
-                }
-                continue;
-            }
-            System.out.println("added: " + line); //add
             System.out.println("--");
+            line = in.nextLine().trim();
+            String[] parts = line.split(" ", 2);
+            String command = parts[0];
+            System.out.println("--");
+            switch (command) {
+            case "list":
+                System.out.println("Here are the tasks in your list meow:");
+                for (int i = 0; i < len; i++) {
+                    System.out.println((i + 1) + ".[" + list[i].getStatusIcon() + "] " + list[i].description);
+                }
+                break;
 
-            //instruction assumes no more than 100 tasks
-            //hence no need to check for index out of range
-            list[len] = line;
-            len++;
+            case "mark":
+                int toMark = Integer.parseInt(parts[1]);
+                list[toMark - 1].markAsDone();
+                System.out.println("Nice! I've marked this task as done meow:");
+                System.out.println("[X] " + list[toMark - 1].description);
+                break;
+
+            case "unmark":
+                int toUnmark = Integer.parseInt(parts[1]);
+                list[toUnmark - 1].markAsNotDone();
+                System.out.println("Aw man, I guess you're not done yet. Do it soon meow:");
+                System.out.println("[ ] " + list[toUnmark - 1].description);
+                break;
+            case "bye":
+                break;
+            default:
+                //default case is to add task
+                System.out.println("added: " + line); //add
+
+                //instruction assumes no more than 100 tasks
+                //hence no need to check for index out of range
+                list[len] = new Task(line);
+                len++;
+                break;
+            }
         } while (!line.equals("bye"));
 
         //loop exits when you type bye
--- src/main/java/Swing.java
+++ src/main/java/Swing.java
@@ -3,9 +3,9 @@
 public class Swing {
     public static void main(String[] args) {
         System.out.println("--");
-        System.out.println("Hello! I'm Swing ^_^");
-        System.out.println("What can I do for you?");
-        System.out.println("--");
+        System.out.println("Hello! I'm Swing~ ^_^");
+        System.out.println("What can I do for you meow?");
+        //System.out.println("--");
 
         List list = new List();
         list.start();

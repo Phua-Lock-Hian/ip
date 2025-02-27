55538e61f8213c0d2f3a784220e4843be0ee461b
Phua-Lock-Hian <e1157347@u.nus.edu>
2025-02-27 12:41:16 +0800

    Edits to code style based on TA feedback

--- src/main/java/swing/Parser.java
+++ src/main/java/swing/Parser.java
@@ -1,6 +1,8 @@
 package swing;
 
-import swing.tasktypes.*;
+import swing.tasktypes.Task;
+import swing.tasktypes.Deadline;
+import swing.tasktypes.Event;
 
 /**
  * Class to parse and execute user commands
@@ -13,17 +15,17 @@ public class Parser {
 
     private boolean isInvalidIndex(int index, TaskList tasks, Ui ui) {
         if (index <= 0 || index > tasks.size()) {
-            errorMessage(ui);
+            printErrorMessage(ui);
             return true;
         }
         return false;
     }
 
-    private void errorMessage(Ui ui) {
+    private void printErrorMessage(Ui ui) {
         ui.showMessage("uh oh, something went wrong meow. try again~");
     }
 
-    private void addedToList(TaskList tasks, Ui ui) {
+    private void addToList(TaskList tasks, Ui ui) {
         ui.showMessage("added: " + tasks.get(tasks.size() - 1).toString()); //to offset zero indexing
         ui.showMessage("Now you have " + tasks.size() + " tasks in list.");
     }
@@ -41,7 +43,7 @@ public class Parser {
             throw new SwingException();
         }
         tasks.add(new Event(eventDesc, start, end));
-        addedToList(tasks, ui);
+        addToList(tasks, ui);
     }
 
     private void processDeadlineCommand(String[] parts, TaskList tasks, Ui ui) throws SwingException {
@@ -58,7 +60,7 @@ public class Parser {
             throw new SwingException();
         }
         tasks.add(new Deadline(deadlineDesc, by));
-        addedToList(tasks, ui);
+        addToList(tasks, ui);
     }
 
     private void processTaskStatus(String[] parts, boolean isDone, TaskList tasks, Ui ui) throws SwingException {
@@ -119,7 +121,7 @@ public class Parser {
         case "list":
             //validation is done outside so that I do not have to pass parts into the process command function
             if (isInvalidCommand(parts, 1)) { //in case user types list followed by some extra text
-                errorMessage(ui);
+                printErrorMessage(ui);
                 break;
             }
             ui.showMessage("Here are the tasks in your list meow:"); //placed outside processListCommand so the former is reusable for "find" case
@@ -129,66 +131,66 @@ public class Parser {
             try {
                 processDeleteCommand(parts, tasks, ui);
             } catch (SwingException e) {
-                errorMessage(ui);
+                printErrorMessage(ui);
             }
             break;
         case "mark":
             try {
                 processTaskStatus(parts, true, tasks, ui);
             } catch (SwingException e) {
-                errorMessage(ui);
+                printErrorMessage(ui);
             }
             break;
         case "unmark":
             try {
                 processTaskStatus(parts, false, tasks, ui);
             } catch (SwingException e) {
-                errorMessage(ui);
+                printErrorMessage(ui);
             }
             break;
         case "deadline":
             try {
                 processDeadlineCommand(parts, tasks, ui);
             } catch (SwingException e) {
-                errorMessage(ui);
+                printErrorMessage(ui);
             }
             break;
         case "event":
             //event command requires more validation than the rest
             //have not thought of a good way to abstract this better, but it is functional this way still
             if (isInvalidCommand(parts, 2)) {
-                errorMessage(ui);
+                printErrorMessage(ui);
                 break;
             }
 
             String[] eventArgParts = parts[1].split("/");
 
             if (isInvalidCommand(eventArgParts, 3)) {
-                errorMessage(ui);
+                printErrorMessage(ui);
                 break;
             }
 
             String eventDesc = eventArgParts[0];
 
             if (!eventArgParts[1].startsWith("from") || !eventArgParts[2].startsWith("to")) {
-                errorMessage(ui);
+                printErrorMessage(ui);
                 break;
             }
             try {
                 processEventCommand(eventArgParts, eventDesc, tasks, ui);
             } catch (SwingException e) {
-                errorMessage(ui);
+                printErrorMessage(ui);
             }
             break;
         case "todo":
             //instruction assumes no more than 100 tasks
             //hence no need to check for index out of range
             if (isInvalidCommand(parts, 2)) {
-                errorMessage(ui);
+                printErrorMessage(ui);
                 break;
             }
             tasks.add(new Task(parts[1]));
-            addedToList(tasks, ui);
+            addToList(tasks, ui);
             break;
         case "find":
             processFindCommand(tasks, ui, parts);
@@ -197,7 +199,7 @@ public class Parser {
             break;
         default:
             //default case is invalid command
-            errorMessage(ui);
+            printErrorMessage(ui);
             break;
         }
     }
